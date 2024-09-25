315983725a698ea98e771fc9c91abe365f9c2e97
duncorir <duncorir@mail.com>
2024-09-25 14:03:09 +0300

    nop

--- nop.c
+++ nop.c
@@ -4,9 +4,14 @@
  * nop - Does nothing
  * @stack: Pointer to the top of the stack
  * @line_number: The current line number in the bytecode file
+ *
+ * This function is an implementation of the NOP opcode,
+ * which stands for "no operation". It does
+ * not alter the stack or output anything.
  */
 void nop(stack_t **stack, unsigned int line_number)
 {
 	(void)stack;
 	(void)line_number;
+
 }
